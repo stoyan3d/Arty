3ffff906c5c6645a8b5c96c909ff6264f663ac71
Stoyan Dimitrov <stoyan3d@gmail.com>
2015-12-24 19:45:34 +0000

    First working version of Arty bot.

--- Arty/Arty.py
+++ Arty/Arty.py
@@ -1,3 +1,14 @@
 import time
-from slackclient._client import SlackClient
-import slackclient from SlackClient
+from slackclient import SlackClient
+#import slackclient from SlackClient
+
+# This token value needs to be external for security reasons
+token = "xoxb-17298431846-I8i5T6PolMevx527rEPZqozM" # found at https://api.slack.com/web#authentication
+sc = SlackClient(token)
+
+if sc.rtm_connect():
+    while True:
+        print(sc.rtm_read())
+        time.sleep(1)
+else:
+    print("Connection failed!")
