9f1b66e39ecb730e93d3a29d8f6339fff4cf21b1
Stoyan Dimitrov <stoyan3d@gmail.com>
2015-12-24 20:21:23 +0000

    Adding a Python 2.7 environment.

--- Arty/Arty.py
+++ Arty/Arty.py
@@ -5,8 +5,11 @@ from slackclient import SlackClient
 # This token value needs to be external for security reasons
 token = "xoxb-17298431846-I8i5T6PolMevx527rEPZqozM" # found at https://api.slack.com/web#authentication
 sc = SlackClient(token)
+channel = "#arty-test"
+greeting = "Hello folks, I'm online!"
 
 if sc.rtm_connect():
+    sc.rtm_send_message([channel, "greeting"])
     while True:
         print(sc.rtm_read())
         time.sleep(1)
