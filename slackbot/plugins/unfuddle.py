b7cd6bc9912097669a858c56717779d533a56588
Stoyan Dimitrov <stoyan3d@gmail.com>
2015-12-29 18:24:11 +0000

    Integrating recent comments and refactoring global variables.

--- slackbot/plugins/unfuddle.py
+++ slackbot/plugins/unfuddle.py
@@ -6,8 +6,20 @@ import re
 unf = Client()
 
 @listen_to('active tickets', re.IGNORECASE)
-def unfuddle_send(message):
+def unfuddle_tickets(message):
 	message.reply('Here are the currently active tickets:')
 	active_tickets = unf.get_active()
 	for i in active_tickets:
 		message.send(i)
+
+@listen_to('recent comments', re.IGNORECASE)
+def unfuddle_comments(message):
+	message.reply('There are some updates:')
+	recent_comments = unf.get_comments()
+	for c in recent_comments:
+		message.send(c)
+
+@listen_to('download', re.IGNORECASE)
+def download(message):
+	message.reply('Downloading files')
+	unf.download()
--- slackbot/plugins/unfuddler/client.py
+++ slackbot/plugins/unfuddler/client.py
@@ -1,6 +1,7 @@
 from unfuddle import Unfuddle
 from settings import ACCOUNT_DETAILS
 import json
+import urllib2
 import logging
 from datetime import datetime, date, timedelta
 
@@ -9,38 +10,44 @@ logger = logging.getLogger(__name__)
 class Client(object):
 	def __init__(self):
 		self.unf = Unfuddle(ACCOUNT_DETAILS["account"], ACCOUNT_DETAILS["username"], ACCOUNT_DETAILS["password"])
+		self.trex_id = '37108'
+		self.ticket_url = self.unf.base_url + '/a#/projects/' + self.trex_id + '/tickets/by_number/'
+		self.downloads = 'D:/Temp/'
 
 	def get_active(self):
 		# Get all active tickets from T-Rex
-		logger.info ('Getting active tickets.')
+		logger.info('Getting active tickets.')
 		active_tickets = []
 		index = 1
-		trex_id = "37108"
-		ticket_url = self.unf.base_url + '/a#/projects/' + trex_id + '/tickets/by_number/'
-		tickets = self.unf.get_tickets(trex_id)
+		tickets = self.unf.get_tickets(self.trex_id)
 		for ticket in tickets:
 			if ticket['status'] == 'Accepted':
 				active_tickets.append(str(index) + '. ' + ticket['summary'] + '\n' 
-					+ ticket_url + str(ticket['number']))
+					+ self.ticket_url + str(ticket['number']))
 				index += 1
-				#print ticket['summary']
 		return active_tickets
 
-
-# Get the most recent comments. These will be update daily.
 	def get_comments(self):
-		trex_id = "37108"
-		tickets = self.unf.get_tickets(trex_id)
+		# Get the most recent comments. These will be updated daily.
+		logger.info('Getting recent comments.')
+		recent_comments = []
+		tickets = self.unf.get_tickets(self.trex_id)
 		current_time = datetime.today() - timedelta(days = 1)
-		print '\n'
-		print 'Recent Comments:'
 		for ticket in tickets:
+			# Some tickets tend to not have comments
 			if ticket.get('comments'):
 				for comment in ticket['comments']:
 					if datetime.strptime(comment['updated_at'], '%Y-%m-%dT%H:%M:%SZ') > current_time:
-						print comment['body']
-						print '-----------------------------------------------------------------'
+						#print comment['body']
+						recent_comments.append(comment['body'])
+		return recent_comments
+
 
 	def print_something(self):
 		logger.info("I'm alive on Python.")
 		return "I'm alive on Slack."
+
+	def download(self):
+		#response = urllib2.urlopen('https://gameshastra.unfuddle.com/projects/37108/tickets/582458/comments/458997/attachments/190940/download', self.downloads + 'something.zip')
+		#html = response.read()
+		self.unf.get('projects/37108/tickets/582458/comments/458997/attachments/190940/download')
