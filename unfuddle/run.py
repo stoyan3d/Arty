ce6aa7854123b9f8a3c1c9006f3645647158e711
Stoyan Dimitrov <stoyan3d@gmail.com>
2016-01-02 16:59:13 +0000

    Updating run file to create a Json archive of all tickets.

--- unfuddle/run.py
+++ unfuddle/run.py
@@ -8,34 +8,34 @@ trex_id = "37108"
 accepted_tickets = []
 
 # Display current projects
-print "Active Projects:"
-projects = unf.get_projects()
-for index, project in enumerate(projects):
-    print '%s. %s' % (index+1, project['title'])
+# print "Active Projects:"
+# projects = unf.get_projects()
+# for index, project in enumerate(projects):
+#     print '%s. %s' % (index+1, project['title'])
 
 # Get all active tickets from T-Rex
 # Need to find a way to query the project ID
 tickets = unf.get_tickets(trex_id)
 
-# with open('tickets.json', 'w') as outfile:
-# 	json.dump(tickets, outfile)
+with open('tickets.json', 'w') as outfile:
+	json.dump(tickets, outfile)
 
-print '\n'
-print 'Active Tickets:'
-for ticket in tickets:
-	if ticket['status'] == 'Accepted':
-		#print datetime.strptime(ticket['updated_at'], '%Y-%m-%dT%H:%M:%SZ').date()
-		#accepted_tickets.append(ticket)
-		print ticket['summary']
+# print '\n'
+# print 'Active Tickets:'
+# for ticket in tickets:
+# 	if ticket['status'] == 'Accepted':
+# 		#print datetime.strptime(ticket['updated_at'], '%Y-%m-%dT%H:%M:%SZ').date()
+# 		#accepted_tickets.append(ticket)
+# 		print ticket['summary']
 
 
-# Get the most recent comments. These will be update daily.
-current_time = datetime.today() - timedelta(days = 1)
-print '\n'
-print 'Recent Comments:'
-for ticket in tickets:
-	if ticket.get('comments'):
-		for comment in ticket['comments']:
-			if datetime.strptime(comment['updated_at'], '%Y-%m-%dT%H:%M:%SZ') > current_time:
-				print comment['body']
-				print '-----------------------------------------------------------------'
+# # Get the most recent comments. These will be update daily.
+# current_time = datetime.today() - timedelta(days = 1)
+# print '\n'
+# print 'Recent Comments:'
+# for ticket in tickets:
+# 	if ticket.get('comments'):
+# 		for comment in ticket['comments']:
+# 			if datetime.strptime(comment['updated_at'], '%Y-%m-%dT%H:%M:%SZ') > current_time:
+# 				print comment['body']
+# 				print '-----------------------------------------------------------------'
