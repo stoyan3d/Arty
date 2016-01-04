a7b09ddbabfd2d38db52c03194da2ff9b0bd79e2
Stoyan Dimitrov <stoyan@spaceapegames.com>
2016-01-04 11:54:38 +0000

    Small tweaks.

--- setup.bat
+++ setup.bat
@@ -3,3 +3,4 @@ virtualenv env1
 call %~dp0env1\Scripts\activate
 cd %~dp0
 pip install -r requirements.txt
+pause
--- slackbot/plugins/hello.py
+++ slackbot/plugins/hello.py
@@ -4,10 +4,10 @@ import re
 
 # The decrators are similar to re.compile('hello$', re.IGNORECASE)
 
-@listen_to('hello|hi')
+@listen_to('hello|hi', re.IGNORECASE)
 def hello_send(message):
     message.send('hello!')
 
-@listen_to('awesome|amazing|great work|nice|cool')
+@listen_to('awesome|amazing|great work|nice|cool', re.IGNORECASE)
 def best_send(message):
 	message.send("Best one yet!")
